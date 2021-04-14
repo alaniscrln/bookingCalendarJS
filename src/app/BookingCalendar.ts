5aa7ddb9e8de7aa3ff82da31b691004f2069bf36
Jose Sanchez <josedeveloper01@gmail.com>
2021-04-14 13:25:01 +0100

    Methods getFirstDayOfMonth, setMonthStructure and its tests. Interface DayI created.

--- src/app/BookingCalendar.ts
+++ src/app/BookingCalendar.ts
@@ -1,6 +1,7 @@
 import { LANGMONTH } from './lang/months';
 import { MonthI } from './Interfaces/MonthI';
 import { LANGDAY } from './lang/days';
+import { DayI } from './Interfaces/DayI';
 
 export class BookingCalendar {
 
@@ -8,18 +9,26 @@ export class BookingCalendar {
      * Language in which the calendar is going to be displayed
      */
     lang: string;
+
     /**
      * Google API Calendar Key
      */
     private readonly key: string;
+
     /**
      * ?
      */
     months = [];
+
     /**
      * ?
      */
     days = []
+
+    /**
+     * ?
+     */
+     currentDate: Date;
 
     /**
      * lang {es/}
@@ -29,14 +38,21 @@ export class BookingCalendar {
         this.key = key;
         this.months = LANGMONTH[this.lang];
         this.days = LANGDAY[this.lang];
+        this.currentDate = new Date();
     }
 
+    /**
+     * Get name of months
+     * return an array of months
+     */
     getMonths(): string[] {
         return this.months;
     }
 
     /**
+     * Get the name of a month indicated by number
      * month {number} Number of a month, starting from 0
+     * return the requested month name
      */
     getMonth(month: number): string {
         if (month >= this.months.length) {
@@ -44,13 +60,19 @@ export class BookingCalendar {
         }
         return this.months[month];
     }
-   
+
+    /**
+     * Get initial letter of the days
+     * return an array of days
+     */
     getDays(): string[] {
         return this.days;
     }
 
     /**
+     * Get the name of a day indicated by number
      * day {number} Number of a day of the week, starting from 0
+     * return the requested day name
      */
     getDay(day: number): string {
         if (day >= this.days.length) {
@@ -60,14 +82,62 @@ export class BookingCalendar {
     }
 
     /**
+     * Get the number of days of a month
      * month {number} Number of the month, starting from 1, which we want to know how many days does it have
      * year {number} Number of the year
+     * return the number of days that the requested month has
      */
     getMonthDays(month: number, year: number): number {
-        if(month <= 0 || month > 12){
+        if (month <= 0 || month > 12) {
             throw new Error('Month cannot be greater than 12 and less than 1.');
         }
         return new Date(year, month, 0).getDate();
     };
 
+    /**
+     * Get the first day of the month as a number
+     * year {number} Number of the year
+     * month {year} Number of the month, starting from 0
+     * return the number of the day, starting from 0 as Sunday
+     */
+    getFirstDayOfMonth(month: number, year: number): number {
+        if (month < 0 || month > 11) {
+            throw new Error('Month cannot be greater than 11 and less than 0.');
+        }
+        var date = new Date(year, month, 1);
+        var index = date.getDay();
+        return index;
+    }
+
+    /**
+     * Set the month structure
+     * date {Date} date 
+     * return an array with the structure of the month
+     */
+    setMonthStructure(date: Date): DayI[] {
+        const month: number = date.getMonth();
+        const year: number = date.getFullYear();
+
+        const blankSpaces: number = this.getFirstDayOfMonth(month, year);
+        const monthDays: number = this.getMonthDays(month + 1, year);
+
+        let monthStructure: DayI[] = new Array(blankSpaces);
+        monthStructure.fill(null, 0, blankSpaces);
+        let days: DayI[] = Array.from({ length: monthDays }, (_, index) => ({day: index + 1+""} as DayI));
+        monthStructure = monthStructure.concat(days);
+
+        return monthStructure;
+    }
+
+    setPreviousMonth(){
+        this.currentDate.setMonth(this.currentDate.getMonth() - 1);
+    }
+    
+    setNextMonth(){
+        this.currentDate.setMonth(this.currentDate.getMonth() + 1);
+    }
+ 
+    changeMonth(date:Date){
+        this.currentDate = date;
+    }
 }
--- src/test/BookingCalendar.spec.ts
+++ src/test/BookingCalendar.spec.ts
@@ -2,6 +2,7 @@ import { expect } from 'chai';
 import 'mocha';
 import { BookingCalendar } from '../app/BookingCalendar';
 
+
 describe("getMonths function from BookingCalendar class", () => {
     it("should return months in spanish", () => {
         const calendar = new BookingCalendar('es', 'key');
@@ -36,6 +37,15 @@ describe("getMonth function from BookingCalendar class", () => {
         expect(result).to.equal(expectedResult);
     });
 
+    it("should return month 5 in spanish but the expected result is in english", () => {
+        const calendar = new BookingCalendar('es', 'key');
+
+        let result: string = calendar.getMonth(5);
+        let expectedResult: string = 'June';
+
+        expect(result).to.not.equal(expectedResult);
+    });
+
     it("should return month 5 in english", () => {
         const calendar = new BookingCalendar('en', 'key');
 
@@ -43,6 +53,15 @@ describe("getMonth function from BookingCalendar class", () => {
         let expectedResult: string = 'June';
 
         expect(result).to.equal(expectedResult);
+    });
+
+    it("should return month 5 in english but the expected result is in spanish", () => {
+        const calendar = new BookingCalendar('en', 'key');
+
+        let result: string = calendar.getMonth(5);
+        let expectedResult: string = 'Junio';
+
+        expect(result).to.not.equal(expectedResult);
     });
 
     it("shouldn't return month 5 in spanish", () => {
@@ -114,31 +133,72 @@ describe("getDay function from BookingCalendar class", () => {
 
 
 describe("getMonthDay function from BookingCalendar class", () => {
-    it("should return the number of days of February",() =>{
+    it("should return the number of days of February", () => {
         const calendar = new BookingCalendar('es', 'key');
         let expectedResult = 28;
         let result = calendar.getMonthDays(2, 2021);
         expect(result).to.equal(expectedResult);
     });
 
-    it("shouldn't return the number of days of the 14th month, it should throw an error",() =>{
+    it("shouldn't return the number of days of the 14th month, it should throw an error", () => {
         const calendar = new BookingCalendar('es', 'key');
         expect(function () {
             calendar.getMonthDays(14, 21);
         }).to.throw(Error, 'Month cannot be greater than 12 and less than 1.');
     });
 
-    it("shouldn't return the number of days of -2 month, it should throw an error",() =>{
+    it("shouldn't return the number of days of -2 month, it should throw an error", () => {
         const calendar = new BookingCalendar('es', 'key');
         expect(function () {
             calendar.getMonthDays(-10, 21);
         }).to.throw(Error, 'Month cannot be greater than 12 and less than 1.');
     });
 
-    it("should return the number of days of February, even if the year is negative",() =>{
+    it("should return the number of days of February, even if the year is negative", () => {
         const calendar = new BookingCalendar('es', 'key');
         let expectedResult = 28;
         let result = calendar.getMonthDays(2, -2021);
         expect(result).to.equal(expectedResult);
-    }); 
+    });
 });
+
+
+describe("getFirstDayOfMonth function from BookingCalendar class", () => {
+    it("should show the number of the first day of August 2021 (Sunday => 0)", () => {
+        const calendar = new BookingCalendar('es', 'key');
+        let result = calendar.getFirstDayOfMonth(7, 2021);
+        let expectedResult = 0;
+        expect(result).to.equal(expectedResult);
+    });
+
+    it("shouldn't return the number of the first day of the 14th month, it should throw an error", () => {
+        const calendar = new BookingCalendar('es', 'key');
+        expect(function () {
+            calendar.getFirstDayOfMonth(14, 2021);
+        }).to.throw(Error, 'Month cannot be greater than 11 and less than 0.');
+    });
+
+
+});
+
+describe("setMonthStructure function from BookingCalendar class", () => {
+    it("", () => {
+        const calendar = new BookingCalendar('es', 'key');
+        const date = new Date(2021, 1);
+        const result = calendar.setMonthStructure(date);
+        const expectedResult = [
+            null,          { day: '1' },  { day: '2' },
+            { day: '3' },  { day: '4' },  { day: '5' },
+            { day: '6' },  { day: '7' },  { day: '8' },
+            { day: '9' },  { day: '10' }, { day: '11' },
+            { day: '12' }, { day: '13' }, { day: '14' },
+            { day: '15' }, { day: '16' }, { day: '17' },
+            { day: '18' }, { day: '19' }, { day: '20' },
+            { day: '21' }, { day: '22' }, { day: '23' },
+            { day: '24' }, { day: '25' }, { day: '26' },
+            { day: '27' }, { day: '28' }
+          ];
+
+        expect(result).to.eql(expectedResult);
+    });
+})
