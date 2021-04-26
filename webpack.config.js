1416c96c0b741bb2ce46f3ca390146eee498ddf8
Jose Sanchez <josedeveloper01@gmail.com>
2021-04-26 11:07:12 +0100

    Setting config. Not finished

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
 const path = require('path');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "development",
-  entry: './src/index.ts',
+  entry:{   
+     app:'./src/index.ts',
+     styles: './src/assets/sass/styles.scss'
+     }, 
   module: {
     rules: [
       {
@@ -10,13 +16,34 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: "css-loader", options: { url: false, importLoaders: 1 } },
+          { loader: 'postcss-loader', options: { plugins: [autoprefixer(), cssnano()] }},
+          { loader: 'sass-loader' },
+        ],
+      }
     ],
   },
   resolve: {
-    extensions: ['.ts', '.ts', '.js'],
+    extensions: ['.ts', '.ts', '.js', '.scss','.css'],
   },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build'),
+/*
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].[chunkhash].chunk.js',
+    path: path.resolve(__dirname, 'css'),
+    publicPath: "/css" */
   },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+      chunkFilename: 'styles.chunk.css',
+    })
+  ]
 };
